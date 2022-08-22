41d916cf99c46ac9ce2ef3b2321dade124548125
Mayur505x <mayurpawar8691@gmail.com>
2022-08-22 12:25:07 +0530

    Added: Lombok Library

--- com/bridgelabz/employeepayrollapp/dto/ResponseDto.java
+++ com/bridgelabz/employeepayrollapp/dto/ResponseDto.java
@@ -1,27 +1,16 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
 public class ResponseDto {
     private String message;
     private Object data;
 
     public ResponseDto(String message, Object data) {
         this.message = message;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
         this.data = data;
     }
 }
