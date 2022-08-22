ea9cc482e2bbf1a7a1ccd18c213292399a3d7497
Mayur505x <mayurpawar8691@gmail.com>
2022-08-22 13:07:05 +0530

    Added: Using @Slf4j Logging

--- com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class EmployeePayrollAppApplication {
+@Slf4j
+public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
     }
-
 }
