031ae3b2f70cb7a83d82bd43938802c59dcf483b
Mayur505x <mayurpawar8691@gmail.com>
2022-08-24 15:43:43 +0530

    Refactored: Validations is done on the Payroll Dto

--- main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionalHandler.java
+++ main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionalHandler.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp.exception;
 import com.bridgelabz.employeepayrollapp.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class EmployeePayrollExceptionalHandler {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
